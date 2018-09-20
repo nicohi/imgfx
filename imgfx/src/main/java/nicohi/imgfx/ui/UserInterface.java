f1d344580f4d1670da8b9b5cff5f387e162a6248
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-09-20 16:44:59 +0300

    Initialize UI

---
+++ imgfx/src/main/java/nicohi/imgfx/ui/UserInterface.java
@@ -0,0 +1,17 @@
+package nicohi.imgfx.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author nicohi
+ */
+public class UserInterface extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+				
+}
