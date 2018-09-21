a93fdd222540394b2ebe8011ed1756f776cf5f98
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-09-21 13:50:55 +0300

    Increase test coverage of PixelSort

--- imgfx/src/test/java/nicohi/imgfx/filters/PixelSortTest.java
+++ imgfx/src/test/java/nicohi/imgfx/filters/PixelSortTest.java
@@ -18,19 +18,42 @@ public class PixelSortTest {
 	}
 
 	@Test
+	public void tSortTest1() {
+		int[][] img1 = {{2,3,1}, {4,2,1}};
+		int[][] img2 = {{1,2,3}, {1,2,4}};
+		PixelSort.tSort(0, img1);
+		assertArrayEquals(img2, img1);
+	}
+	
+	@Test
 	public void redSortTest1() {
 		int[][] img1 = {{2,3,1}, {4,2,1}};
 		int[][] img2 = {{1,2,3}, {1,2,4}};
 		PixelSort.redSort(img1);
-		assertEquals(img1, img2);
+		assertArrayEquals(img2, img1);
 	}
 
+	@Test
+	public void copyImgTest1() {
+		int[][] img2 = {{1,2,3}, {1,2,4}};
+		int[][] img1 = PixelSort.copyImg(img2);
+		assertArrayEquals(img2, img1);
+	}
+
+	@Test
+	public void copyImgToTest1() {
+		int[][] img2 = {{1,2,3}, {1,2,4}};
+		int[][] img1 = {{0,3,3}, {2,2,3}};
+		PixelSort.copyTo(img2, img1);
+		assertArrayEquals(img2, img1);
+	}
+	
 	@Test
 	public void sortRangeTest1() {
 		int[] img1 = {2,3,1,1};
 		int[] img2 = {1,2,3,1};
-		PixelSort.sortRange(0, 2, img1);
-		assertEquals(img1, img2);
+		PixelSort.sortRange(0, 3, img1);
+		assertArrayEquals(img2, img1);
 	}
 
 	@Test
@@ -38,8 +61,8 @@ public class PixelSortTest {
 		int[][] img1 = {{1,3,0}, {1,0,2}};
 		int[][] img2 = {{0xFF000000,0xFFFFFFFF,0xFF000000}, {0xFF000000,0xFF000000,0xFFFFFFFF}};
 		PixelSort.applyF(PixelSort.binaryThreshold(2), img1);
-		assertEquals(img1[0], img2[0]);
-		assertEquals(img1[1], img2[1]);
+		assertArrayEquals(img2[0], img1[0]);
+		assertArrayEquals(img2[1], img1[1]);
 	}
 	
 }
