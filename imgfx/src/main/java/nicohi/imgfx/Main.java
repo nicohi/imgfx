9cef4c62d167b8379146d98335811e6d5de5e6e5
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-10-05 21:55:54 +0300

    Add 'bench' option

--- imgfx/src/main/java/nicohi/imgfx/Main.java
+++ imgfx/src/main/java/nicohi/imgfx/Main.java
@@ -4,6 +4,7 @@ import nicohi.imgfx.Picture;
 import java.io.File;
 import java.net.URL;
 import java.util.Arrays;
+import nicohi.imgfx.benchmark.PixelSortBenchmark;
 import nicohi.imgfx.filters.GaussBlur;
 import nicohi.imgfx.filters.PixelSort;
 
@@ -19,18 +20,29 @@ public class Main {
 				URL f = new File(args[1]).toURI().toURL();
 				int[][] pic = Picture.readFromPath(f);
 				System.out.println(pic.length + " " + pic[0].length);
-				PixelSort.tSort(Integer.parseInt(args[2]), pic);
+				PixelSort.pixelMergeSortThreshold(Integer.parseInt(args[2]), pic);
 				Picture.writeToFile(new File("out.png"), pic);
 			}
 			if (args[0].equals("gblur")) {
 				URL f = new File(args[1]).toURI().toURL();
 				int[][] pic = Picture.readFromPath(f);
 				//System.out.println(Arrays.toString(pic));
-				int[][] res = GaussBlur.applyKernel1D(pic, GaussBlur.kernel1D(Integer.parseInt(args[2])));
+				int[][] res = GaussBlur.gaussianBlur1D(pic, Integer.parseInt(args[2]), Integer.parseInt(args[3]));
 				//int[][] res = GaussBlur.applyKernel1D(Picture.rotateRight(pic), GaussBlur.kernel1D(Double.parseDouble(args[2])));
 				//System.out.println(pic.length + " " + pic[0].length);
 				//System.out.println(Arrays.toString(GaussBlur.kernel1D(Double.parseDouble(args[1]))));
 				Picture.writeToFile(new File("out.png"), res);
+			}
+//			if (args[0].equals("rrot")) {
+//				URL f = new File(args[1]).toURI().toURL();
+//				int[][] pic = Picture.readFromPath(f);
+//				int[][] res = Picture.rotateLeft(pic);
+//				Picture.writeToFile(new File("out.png"), res);
+//			}
+			if (args[0].equals("bench")) {
+				URL f = new File(args[1]).toURI().toURL();
+				int[][] img = Picture.readFromPath(f);
+				PixelSortBenchmark.mergeVsSelSort(img);
 			}
 			
 		} catch (Exception ex) {
