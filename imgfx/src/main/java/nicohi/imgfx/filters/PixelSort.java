677e7dd4b58311c23d1a6e418e21df9854fd489f
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-10-05 21:53:10 +0300

    Add merge and sel sort

--- imgfx/src/main/java/nicohi/imgfx/filters/PixelSort.java
+++ imgfx/src/main/java/nicohi/imgfx/filters/PixelSort.java
@@ -26,7 +26,7 @@ public class PixelSort {
 	 * @param tr
 	 * @param img
 	 */
-	public static void tSort(int tr, int[][] img) {
+	public static void pixelSelSortThreshold(int tr, int[][] img) {
 		int[][] mask = Picture.copyImg(img);
 		applyF(binaryThreshold(tr), mask);
 		
@@ -44,19 +44,48 @@ public class PixelSort {
 				if ((rgb == 0 || x == mask[0].length - 1) && s) {
 					t = x;
 					s = false;
-					sortRange(b, t+1, img[y]);
+					selSortRange(b, t+1, img[y]);
 				}
 			}
 		}
 	}
 
 	/**
-	 * Sorts a range of indices in an array.
+	 * Apply pixel sort to an image with a brightness threshold.
+	 * @param tr
+	 * @param img
+	 */
+	public static void pixelMergeSortThreshold(int tr, int[][] img) {
+		int[][] mask = Picture.copyImg(img);
+		applyF(binaryThreshold(tr), mask);
+		
+		for (int y = 0; y < mask.length; y++) {
+			int b = 0;
+			int t = img[y].length;
+			boolean s = true;
+			for (int x = 0; x < mask[0].length; x++) {
+				int rgb = mask[y][x] & 0xFFFFFF;
+				//System.out.println(Integer.toHexString(rgb));
+				if (rgb != 0 && !s) {
+					b = x;
+					s = true;
+				}
+				if ((rgb == 0 || x == mask[0].length - 1) && s) {
+					t = x;
+					s = false;
+					mergeSortRange(b, t+1, img[y]);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Sorts a range of indices in an array with selection sort.
 	 * @param b
 	 * @param t
 	 * @param row
 	 */
-	public static void sortRange(int b, int t, int[] row) {
+	public static void selSortRange(int b, int t, int[] row) {
 		//TODO better sort
 		//System.out.println("sorting range "+b+"-"+t);
 		if (b >= t) return;
@@ -70,7 +99,66 @@ public class PixelSort {
 		}
 		row[from] = row[b];
 		row[b] = min;
-		sortRange(b + 1, t, row);
+		selSortRange(b + 1, t, row);
+	}
+
+	/**
+	 * Sorts a range of indices in an array with merge sort.
+	 * @param b
+	 * @param t
+	 * @param row
+	 */
+	public static void mergeSortRange(int b, int t, int[] row) {
+		setRange(b, mergeSort(copyRange(b, t, row)), row);
+	}
+
+	public static int[] mergeSort(int[] l) {
+		if (l.length <= 1) return l;
+		int len = l.length / 2;
+		return merge(mergeSort(copyRange(0, len, l)),
+					 mergeSort(copyRange(len, l.length, l)));
+	}
+
+	public static int[] merge(int[] l1, int[] l2) {
+		//if (l1.length == 0) return l2;
+		//if (l2.length == 0) return l1;
+		int len = l1.length + l2.length;
+		int[] res = new int[len];
+		int i1 = 0;
+		int i2 = 0;
+		int ir = 0;
+		while (ir < res.length) {
+			if (i1 == l1.length) {
+				res[ir] = l2[i2];
+				i2++;
+				ir++;
+				continue;
+			} else if (i2 == l2.length) {
+				res[ir] = l1[i1];
+				i1++;
+				ir++;
+				continue;
+			}
+			if (l1[i1] < l2[i2]) {
+				res[ir] = l1[i1];
+				i1++;
+			} else {
+				res[ir] = l2[i2];
+				i2++;
+			}
+			ir++;
+		}
+		return res;
+	}
+
+	public static void setRange(int b, int[] rowSeg, int[] row) {
+		for (int i = b; i < rowSeg.length + b; i++) row[i] = rowSeg[i - b];
+	}
+
+	public static int[] copyRange(int b, int t, int[] row) {
+		int[] res = new int[t - b];
+		for (int i = b; i < t; i++) res[i - b] = row[i];
+		return res;
 	}
 
 	/**
